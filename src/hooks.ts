c20e63813b8ef5d95b32d667cc6133841f1c094e
Rajko Vukovic <rajkovukovic@gmail.com>
2022-01-25 20:16:51 +0100

    project setup

---
+++ src/hooks.ts
@@ -0,0 +1,9 @@
+import type { Handle } from '@sveltejs/kit';
+
+export async function handle({ event, resolve }): Promise<Handle> {
+	const response = await resolve(event, {
+		ssr: false
+	});
+
+	return response;
+}
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<main>
+	<header>
+		<nav>
+			<a sveltekit:prefetch href="/">Home</a>
+			<a sveltekit:prefetch href="/users">Users</a>
+			<a sveltekit:prefetch href="/help">Help</a>
+		</nav>
+	</header>
+
+	<article>
+		<slot />
+	</article>
+
+	<footer>Footer</footer>
+</main>
