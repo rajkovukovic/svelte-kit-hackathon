4e361fb706e9d4496d87db5f125f55f9b040791a
Rajko Vukovic <rajkovukovic@gmail.com>
2022-01-26 12:24:47 +0100

    add user widget

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import type { Handle} from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit';
 
 export async function handle({ event, resolve }): Promise<Handle> {
 	const response = await resolve(event, {
