69bc4b573c401ce0e9e427f1fa3d68ad2a34b815
Rajko Vukovic <rajkovukovic@gmail.com>
2022-01-26 12:23:50 +0100

    add styling

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -13,3 +13,39 @@
 
 	<footer>Footer</footer>
 </main>
+
+<style>
+	main {
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+		grid-template-columns: 1fr;
+		min-height: 100%;
+	}
+
+	header {
+		position: sticky;
+		top: 0;
+		background-color: #252526;
+	}
+
+	footer {
+		position: sticky;
+		bottom: 0;
+		background-color: #007acc;
+		padding: 1rem;
+	}
+
+	article {
+		padding: 1rem;
+	}
+
+	nav {
+		display: flex;
+	}
+
+	a {
+		display: inline-block;
+		background-color: rgba(255, 255, 255, 0.1);
+		padding: 1rem;
+	}
+</style>
