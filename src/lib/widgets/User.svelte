bb75ecda8d6a9fb673abf11a6efa1da847fcde89
Rajko Vukovic <rajkovukovic@gmail.com>
2022-01-26 12:24:47 +0100

    add user widget

---
+++ src/lib/widgets/User.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let id: string;
+	export let name: string;
+</script>
+
+<div>
+	<img alt="user-avatar" src="https://avatars.dicebear.com/api/adventurer/{id}.svg" />
+	<i>@{id}</i>
+	<b>{name}</b>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 4px;
+		background-color: rgba(255, 255, 255, 0.1);
+		border-radius: 10px;
+	}
+
+	img {
+		width: 64px;
+		height: 64px;
+	}
+</style>
--- src/routes/users/index.svelte
+++ src/routes/users/index.svelte
@@ -1,1 +1,40 @@
+<script lang="ts">
+	import User from '$lib/widgets/User.svelte';
+
+	let users = [
+		{
+			id: '1',
+			name: 'John Doe'
+		},
+		{
+			id: '2',
+			name: 'Jane Doe'
+		}
+	];
+</script>
+
 <h1>Users</h1>
+<div class="user-list">
+	{#each users as { id, name } (id)}
+		<div class="user-wrapper">
+			<User {id} {name} />
+		</div>
+	{/each}
+</div>
+
+<style>
+	.user-list {
+		display: flex;
+	}
+
+	.user-wrapper {
+		display: flex;
+    flex-direction: column;
+		justify-content: stretch;
+		align-items: stretch;
+		width: 200px;
+		min-height: 150px;
+		margin-bottom: 8px;
+		margin-right: 8px;
+	}
+</style>
