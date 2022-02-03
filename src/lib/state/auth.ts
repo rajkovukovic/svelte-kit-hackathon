ad886a5a051a9e75fd42c83fc847a0f4f77c3923
Rajko Vukovic <rajkovukovic@gmail.com>
2022-02-03 12:51:19 +0100

    Implement custom svelte store

--- src/lib/state/auth.ts
+++ src/lib/state/auth.ts
@@ -1,5 +1,35 @@
-import { writable } from "svelte/store";
+import type { Subscriber, Unsubscriber, Updater, Writable } from 'svelte/store';
 
-export const authUser = writable<string>(null);
+class SimpleStore<T> implements Writable<T> {
+	private _value: T;
+	private _listeners: Subscriber<T>[];
 
-authUser.subscribe(username => console.log('authUser:', username));
+	constructor(initialValue: T) {
+		this._value = initialValue;
+		this._listeners = [];
+	}
+
+	set(nextValue: T): void {
+		this._value = nextValue;
+		this._listeners.forEach((listener) => listener(nextValue));
+	}
+
+	update(updater: Updater<T>): void {
+		const nextValue = updater(this._value);
+		this.set(nextValue);
+	}
+
+	subscribe(listener: Subscriber<T>, invalidate?: (value?: T) => void): Unsubscriber {
+		this._listeners.push(listener);
+		return () => {
+			const index = this._listeners.indexOf(listener);
+			if (index >= 0) {
+				this._listeners.splice(index);
+			}
+		};
+	}
+}
+
+export const authUser = new SimpleStore<string>(null);
+
+authUser.subscribe((username) => console.log('authUser:', username));
